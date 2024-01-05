e9a720ebeefa79570eb133c929d53ff110134c75
marek_eti <student@KOMP26>
2024-01-05 15:06:21 +0100

    men

--- jpwp/Form1.Designer.cs
+++ jpwp/Form1.Designer.cs
@@ -28,74 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            scoreScreen = new Label();
-            gameTimer = new System.Windows.Forms.Timer(components);
-            button1 = new Button();
-            contextMenuStrip1 = new ContextMenuStrip(components);
-            player_panel = new Panel();
-            SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.scoreScreen = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.player_panel = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
             // 
             // scoreScreen
             // 
-            scoreScreen.AutoSize = true;
-            scoreScreen.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            scoreScreen.Location = new Point(10, 689);
-            scoreScreen.Name = "scoreScreen";
-            scoreScreen.Size = new Size(60, 21);
-            scoreScreen.TabIndex = 0;
-            scoreScreen.Text = "wynik:";
+            this.scoreScreen.AutoSize = true;
+            this.scoreScreen.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.scoreScreen.Location = new System.Drawing.Point(10, 689);
+            this.scoreScreen.Name = "scoreScreen";
+            this.scoreScreen.Size = new System.Drawing.Size(60, 21);
+            this.scoreScreen.TabIndex = 0;
+            this.scoreScreen.Text = "wynik:";
             // 
             // gameTimer
             // 
-            gameTimer.Interval = 20;
-            gameTimer.Tag = "gameTimer";
-            gameTimer.Tick += MainGameTimer;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tag = "gameTimer";
             // 
             // button1
             // 
-            button1.Enabled = false;
-            button1.Font = new Font("Showcard Gothic", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.ForeColor = Color.IndianRed;
-            button1.Location = new Point(962, 671);
-            button1.Margin = new Padding(3, 2, 3, 2);
-            button1.Name = "button1";
-            button1.Size = new Size(123, 39);
-            button1.TabIndex = 2;
-            button1.Text = "MENU";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += player_Click;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.IndianRed;
+            this.button1.Location = new System.Drawing.Point(962, 671);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "MENU";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // contextMenuStrip1
             // 
-            contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(61, 4);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // player_panel
             // 
-            player_panel.Location = new Point(489, 630);
-            player_panel.Name = "player_panel";
-            player_panel.Size = new Size(80, 80);
-            player_panel.TabIndex = 3;
-            player_panel.Paint += player_panel_Paint;
+            this.player_panel.Location = new System.Drawing.Point(489, 630);
+            this.player_panel.Name = "player_panel";
+            this.player_panel.Size = new System.Drawing.Size(80, 80);
+            this.player_panel.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(370, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(380, 380);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(132, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(1104, 733);
-            Controls.Add(player_panel);
-            Controls.Add(button1);
-            Controls.Add(scoreScreen);
-            Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "matemars";
-            Load += Form1_Load;
-            KeyDown += KeyIsDown;
-            KeyUp += KeyIsUp;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1104, 733);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.player_panel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scoreScreen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "matemars";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -106,5 +129,7 @@
         private ContextMenuStrip contextMenuStrip1;
         private Panel panel1;
         private Panel player_panel;
+        private Panel panel2;
+        private Label label1;
     }
 }
